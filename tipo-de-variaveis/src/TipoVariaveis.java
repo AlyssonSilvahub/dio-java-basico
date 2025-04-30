0b5226d8dbba1b80d56e71b5c982b9031a011c91
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2025-04-30 01:42:09 -0300

    Remove conteúdo inicial do README e ajuste no código da classe TipoVariaveis

--- tipo-de-variaveis/src/TipoVariaveis.java
+++ tipo-de-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,15 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Inicio Projeto");
+      // double salarioMinimo = 2.500;
+
+     //  short numeroCurto = 1;
+      // int numeroNormal = numeroCurto;
+     //  short numeroCurto2 = (short) numeroNormal;
+       int numero = 1;
+       numero = 5;
+       System.out.println(numero);
+
+      final double VALOR_DE_PI = 3.14;
+       
     }
 }
