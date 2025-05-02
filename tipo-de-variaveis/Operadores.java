e01d094a195cd3e52b0d2c9330ccb7dfdca6e50b
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2025-05-02 02:07:50 -0300

    Remove exemplo de concatenação de strings e ajusta a impressão de número na classe Operadores

--- tipo-de-variaveis/Operadores.java
+++ tipo-de-variaveis/Operadores.java
@@ -1,6 +1,9 @@
 public class Operadores {
     public static void main(String[] args) {
-    String nomeCompleto = "Linguagem" + "JAVA";
-    System.out.println(nomeCompleto);
-}
+    //String nomeCompleto = "Linguagem" + "JAVA";
+    //System.out.println(nomeCompleto);
+    int numero = 5;
+    System.out.println(- numero);
+ }
+
 }
