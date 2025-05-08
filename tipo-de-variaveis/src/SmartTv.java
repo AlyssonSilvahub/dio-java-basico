e7d6900afba814a8da3fca80e8c5a53f8ce591f8
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2025-05-08 02:40:33 -0300

    Ajusta a formatação do método mudarCanal na classe SmartTv

--- tipo-de-variaveis/src/SmartTv.java
+++ tipo-de-variaveis/src/SmartTv.java
@@ -2,7 +2,8 @@ public class SmartTv {
    boolean ligada=false;
    int canal = 1;
    int volume= 7;
-public void mudarCanal(int novoCanal){
+
+   public void mudarCanal(int novoCanal){
     canal = novoCanal;
 }
    public void aumentarCanal(){
