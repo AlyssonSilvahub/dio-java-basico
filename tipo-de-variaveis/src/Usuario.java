d1cd582bbc0fa92e800c9507cea7f3729531d20c
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2025-05-08 01:39:43 -0300

    Ajusta formatação das mensagens de saída na classe Usuario

--- tipo-de-variaveis/src/Usuario.java
+++ tipo-de-variaveis/src/Usuario.java
@@ -3,9 +3,9 @@ public class Usuario {
         SmartTv smartTv = new SmartTv();
 
 
-        System.out.println("Tv Ligada ?" + smartTv.ligada);
-        System.out.println("Canal Atual" + smartTv.canal);
-        System.out.println("Volume Atual" + smartTv.volume);
+        System.out.println("Tv Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual " + smartTv.canal);
+        System.out.println("Volume Atual " + smartTv.volume);
 
 
 
