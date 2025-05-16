5701e2262480d05ba0c0302d72a836aaebddb73d
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2025-05-16 02:39:03 -0300

    Refatora a lógica de exibição de alunos na classe ExemploForArrays para utilizar enhanced for loop

--- tipo-de-variaveis/controle-fluxo/src/ExemploForArrays.java
+++ tipo-de-variaveis/controle-fluxo/src/ExemploForArrays.java
@@ -2,8 +2,8 @@ public class ExemploForArrays {
     public static void main(String[] args) {
         String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
-	for (int x=0; x<alunos.length; x++) {
-		System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+	for (String aluno : alunos) {
+		System.out.println("Nome do aluno: " + aluno);
     
 }
 }
