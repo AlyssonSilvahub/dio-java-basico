8eebb139b14e5eaf9caa6f022b3a99fcb223358c
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2025-05-27 03:35:13 -0300

    Corrige a impressão do índice dos candidatos na lista de seleção

--- tipo-de-variaveis/controle-fluxo/cases/ProcessoSeletivo.java
+++ tipo-de-variaveis/controle-fluxo/cases/ProcessoSeletivo.java
@@ -6,7 +6,11 @@ public class ProcessoSeletivo {
         String [] candidatos = {"Felipe","Alex","João", "Alysson", "Rafael"};   
         System.out.println("Imprimindo a lista de candidatos informando o indice de elementos");
         for (int indice=0; indice < candidatos.length; indice++){
-            System.out.println("O candidato de n° " + indice + " é " + candidatos[indice]);
+            System.out.println("O candidato de n° " + (indice+1) + " é " + candidatos[indice]);
+        }
+        System.out.println("Forma abreviada");
+        for (String candidato: candidatos){
+            System.out.println("O candidato selecionado foi: " + candidato);
         }
     }
     static void selecaoCandidatos() {
