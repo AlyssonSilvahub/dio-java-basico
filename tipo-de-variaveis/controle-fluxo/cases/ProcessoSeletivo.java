ec3b654bf310ea00c21c81d8fba1dcd4fc87273b
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2025-05-27 03:30:21 -0300

    Adiciona lógica para seleção de candidatos e impressão da lista de selecionados

--- tipo-de-variaveis/controle-fluxo/cases/ProcessoSeletivo.java
+++ tipo-de-variaveis/controle-fluxo/cases/ProcessoSeletivo.java
@@ -1,10 +1,36 @@
+import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
-    public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
-        
+    public static void main(String[] args) {      
     }
+    static void imprimirSelecionados() {
+        String [] candidatos = {"Felipe","Alex","João", "Alysson", "Rafael"};   
+        System.out.println("Imprimindo a lista de candidatos informando o indice de elementos");
+        for (int indice=0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de n° " + indice + " é " + candidatos[indice]);
+        }
+    }
+    static void selecaoCandidatos() {
+        String [] candidatos = {"Felipe","Alex","João", "Alysson", "Rafael","Erick","Roberto"};
+           
+         int candidaosSelecionados = 0;
+         int candidastoAtual=0;
+         double salarioBase=2000.0;
+            while(candidaosSelecionados < 5 && candidastoAtual < candidatos.length) {
+                String candidato = candidatos[candidastoAtual];
+                double salarioPretendido = valorPretendido();
+                System.out.println("O candidato" + candidato + "Solicitou este valor de salario" + salarioPretendido);
+                if (salarioBase >= salarioPretendido) {
+                    System.out.println("Candidato " + candidato + " foi selecionado");
+                    candidaosSelecionados++;
+                };
+                candidastoAtual++;
+            };           
+    };
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
+    };
+    
+
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido) {
